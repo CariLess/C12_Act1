86ab6b3de743cc947778abcd99d64f43bf8e83ed
CariLess <90604545+CariLess@users.noreply.github.com>
2021-10-13 12:04:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var altura_BHA = [187,185,177,173,170,168,166,158,154,150,186,179,176,172,169,168,159,156,154,108,180];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+for( var i = 0; i<altura_BHA.length; i++){
+  
+}
+
+}
+function draw() 
+{
+  background(30);
+}
+
+ 
+
